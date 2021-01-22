b01ce426b11421e551237968ccf0f109b2872c2c
Mathias Storm <mathias@storm.lc>
2021-01-22 22:34:20 +0100

    add ability to add simple transactions to ynab

--- dist/daemon.js
+++ dist/daemon.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log('hello world');
+Object.defineProperty(exports, "__esModule", { value: true });
+const ynab_1 = require("ynab");
+const dotenv_1 = require("dotenv");
+const process_1 = require("process");
+dotenv_1.config();
+const budgetId = process.env.YAIT_YNAB_BUDGET;
+const apiKey = process.env.YAIT_YNAB_APIKEY;
+const checkingAccountId = process.env.YAIT_YNAB_CHECKING;
+if (!apiKey || !budgetId || !checkingAccountId) {
+    process_1.exit();
+}
+(async function () {
+    const ynab = new ynab_1.API(apiKey);
+    try {
+        await ynab.transactions.createTransaction(budgetId, {
+            transaction: {
+                account_id: checkingAccountId,
+                amount: 13370,
+                approved: false,
+                date: ynab_1.utils.getCurrentDateInISOFormat(),
+                cleared: ynab_1.SaveTransaction.ClearedEnum.Uncleared,
+            },
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+})();
--- src/daemon.ts
+++ src/daemon.ts
@@ -1,1 +1,30 @@
-console.log('hello world');
+import { API, SaveTransaction, TransactionsApiFactory, utils } from "ynab";
+import {config} from "dotenv";
+import { exit } from "process";
+config();
+
+const budgetId = process.env.YAIT_YNAB_BUDGET
+const apiKey = process.env.YAIT_YNAB_APIKEY
+const checkingAccountId = process.env.YAIT_YNAB_CHECKING;
+
+if(!apiKey || !budgetId || !checkingAccountId) {
+  exit();
+}
+
+(async function () {
+  const ynab = new API(apiKey);
+
+  try {
+    await ynab.transactions.createTransaction(budgetId, {
+      transaction: {
+        account_id: checkingAccountId,
+        amount: 13370,
+        approved: false,
+        date: utils.getCurrentDateInISOFormat(),
+        cleared: SaveTransaction.ClearedEnum.Uncleared,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
